36b4824dbaf31c0d7a09d6ebcdf88f573d503014
balaji <balaji@adaptinsurance.com>
2025-03-01 14:10:38 -0800

    fix vercel deploy issue

--- frontend/next-app/temp-next-app/research-data-viewer/next.config.js
+++ frontend/next-app/temp-next-app/research-data-viewer/next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com'],
   },
 }
 
